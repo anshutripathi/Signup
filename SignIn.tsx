54a33a0f6488bd970e5ce3293586c1d0138e1b0f
rishagarwal1207 <31099159+rishagarwal1207@users.noreply.github.com>
2020-06-20 23:32:04 +0530

    Add files via upload

--- SignIn.tsx
+++ SignIn.tsx
@@ -27,13 +27,7 @@ function Copyright() {
         </Typography>
     );
 }
-const images = [
-    {
-        alignItems :"center",
-        title: 'SIGN UP',
-        width: '40%',
-    },
-];
+
 const useStyles = makeStyles((theme) => ({
         root: {
             
@@ -50,13 +44,16 @@ const useStyles = makeStyles((theme) => ({
                 theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            
+
         },
+        
         paper: {
+            
             margin: theme.spacing(8, 4),
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            
         },
         avatar: {
             margin: theme.spacing(1),
@@ -71,83 +68,12 @@ const useStyles = makeStyles((theme) => ({
             margin: theme.spacing(3, 0, 2),
             backgroundColor: "orange",
         },
-
-        image1: {
-            position: 'absolute',
-            top:'440px',
-            right:'300px',
-            left:'100px',
-            height: 200,
-            
-            [theme.breakpoints.down('xs')]: {
-                width: '100% !important', // Overrides inline-style
-                height: 100,
-            },
-            '&:hover, &$focusVisible': {
-                zIndex: 1,
-                '& $imageBackdrop': {
-                    opacity: 0.15,
-                },
-                '& $imageMarked': {
-                    opacity: 0,
-                },
-                '& $imageTitle': {
-                    border: '4px solid currentColor',
-                },
-            },
-        },
         focusVisible: {},
-        imageButton: {
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: theme.palette.common.white,
-        },
-        imageSrc: {
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center 40%',
-        },
-        imageBackdrop: {
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            backgroundColor: theme.palette.common.black,
-            opacity: 0.4,
-            transition: theme.transitions.create('opacity'),
-        },
-
-        imageTitle: {
-            position: 'relative',
-            padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
-        },
-        imageMarked: {
-            height: 3,
-            width: 18,
-            backgroundColor: theme.palette.common.white,
-            position: 'absolute',
-            bottom: -2,
-            left: 'calc(50% - 9px)',
-            transition: theme.transitions.create('opacity'),
-        },
-        ButtonBase: {
-            alignItems:'center',
-        },
         frame: {
             margin: theme.spacing(60),
                 backgroundColor: 'black',
                 color:'white',
+                opacity:1,
         },
         r: {
             '& label': {
@@ -169,45 +95,11 @@ export default function SignIn() {
     const classes = useStyles();
 
     return (
-        <Grid container component="main" className={classes.root}>
+        <Grid container  className={classes.root}>
             <CssBaseline />
             <Grid item xs={false} sm={4} md={12} className={classes.image} >
             <Grid item xs={10} sm={5} md={5} className={classes.frame} component={Paper} elevation={6} square>
-                {images.map((image) => (
-                <Link href="https://www.google.com" > <ButtonBase
-                    focusRipple
-                    key={image.title}
-                    className={classes.image1}
-                    focusVisibleClassName={classes.focusVisible}
-                    style={{
-                        width: image.width,
-                    }}
-                >
-                    <span
-                        className={classes.imageSrc}
-                        style={{
-                            backgroundImage: "inherit",
-                        }}
-                    />
-                    <span className={classes.imageBackdrop} />
-                    <span className={classes.imageButton}>
-                        <Typography
-                            component="span"
-                            variant="subtitle1"
-                            color="inherit"
-                            className={classes.imageTitle}
-                        >
-                            {image.title}
-                            <span className={classes.imageMarked} />
-                        </Typography>
-                    </span>
-                </ButtonBase> </Link>
-                ))},
-
-
-           
-            
-                <div className={classes.paper}>
+             <div className={classes.paper}>
                     <Avatar className={classes.avatar}>
                         <LockOutlinedIcon />
                     </Avatar>
@@ -251,6 +143,7 @@ export default function SignIn() {
                             control={<Checkbox value="remember" style={{color:'orange'}}  />}
                             label="Remember me"
                         />
+                        <Link href="https://www.google.com">
                             <Button
                             type="submit"
                             fullWidth
@@ -258,18 +151,20 @@ export default function SignIn() {
                             style={{backgroundColor:'orange'}}
                             className={classes.submit}
                         >LOGIN
-                        </Button>
+                        </Button></Link>
 
                         <Grid container >
                             <Grid item xs>
-                                <Link href="#" variant="body2" style={{color:'white'}}>
+                                <Link href="https://www.google.com" variant="body2" style={{color:'white'}}>
                                     Forgot password?
                                 </Link>
                             </Grid>
                             <Grid item>
-                            <Link href="#" variant="body2" style={{color:'white'}}>
-                                    {"Don't have an account? Sign Up"}
+                                <Typography>Don't have an account? </Typography>  
+                                <Link href="#" variant="body2" style={{color:'white'}}>
+                                        { " Sign Up "}
                                 </Link>
+                                
                             </Grid>
                         </Grid>
                         <Box mt={5}>
