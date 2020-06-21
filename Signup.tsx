823bc7315f83d32296fc930c74143656748fb724
Anshu Tripathi <57445864+anshutripathi@users.noreply.github.com>
2020-06-21 13:37:15 +0530

    Add files via upload

--- Signup.tsx
+++ Signup.tsx
@@ -3,23 +3,27 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
-import FunctionsIcon from '@material-ui/icons/Functions';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles';
-
 import { orange } from '@material-ui/core/colors';
+//import SignUp from "./SignUp";
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
         </Typography>
     );
 }
@@ -33,6 +37,7 @@ const useStyles = makeStyles((theme) => ({
         color:'white',
     },
 
+
     paper: {
 
         margin: theme.spacing(8, 4),
@@ -41,19 +46,9 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
 
     },
-
-    ava1: {
-        margin: theme.spacing(10),
-        display: 'flex',
-        '& > *': {
-            margin: theme.spacing(6),
-            color: 'orange',
-        },
-    },
     avatar: {
         margin: theme.spacing(1),
-        backgroundColor: "orange",
-        alignItems: 'center center',
+        backgroundColor:'orange',
     },
 
     form: {
@@ -66,7 +61,7 @@ const useStyles = makeStyles((theme) => ({
     },
     focusVisible: {},
     frame: {
-        margin: theme.spacing(50),
+        margin: theme.spacing(60),
         backgroundColor: 'black',
         color:'white',
         opacity:1,
@@ -93,32 +88,16 @@ export default function SignIn() {
     return (
         <Grid container  className={classes.root}>
             <CssBaseline />
-
-                <Grid item  className={classes.frame} component={Paper} elevation={6} square>
+                <Grid item className={classes.frame} component={Paper} elevation={6} square>
                     <div className={classes.paper}>
                         <Avatar className={classes.avatar}>
-                            <InsertEmoticonIcon/>
+                            <LockOutlinedIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
-                            SIGN UP
+                            LOGIN
                         </Typography>
                         <form className={classes.form} noValidate >
                             <ThemeProvider theme={theme}>
-                                <TextField
-                                    variant="outlined"
-                                    className={classes.r}
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="full name"
-                                    InputProps={{
-                                        className: classes.multilineColor
-                                    }}
-                                    label="Full Name"
-                                    name="full name"
-                                    autoComplete="full name"
-                                    autoFocus
-                                />
                                 <TextField
                                     variant="outlined"
                                     className={classes.r}
@@ -132,8 +111,8 @@ export default function SignIn() {
                                     label="Email Address"
                                     name="email"
                                     autoComplete="email"
+                                    autoFocus
                                 />
-
                                 <TextField
                                     variant="outlined"
                                     className={classes.r}
@@ -149,47 +128,33 @@ export default function SignIn() {
                                     id="password"
                                     autoComplete="current-password"
                                 />
-                                <TextField
-                                    variant="outlined"
-                                    className={classes.r}
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    InputProps={{
-                                        className: classes.multilineColor
-                                    }}
-                                    name="confirm password"
-                                    label="confirm password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
                             </ThemeProvider>
+                            <FormControlLabel
+                                control={<Checkbox value="remember" style={{color:'orange'}}  />}
+                                label="Remember me"
+                            />
 
-                            <Link href="https://www.google.com">
-                                <Button
+                            <Button href="#"
                                     type="submit"
                                     fullWidth
                                     variant="contained"
                                     style={{backgroundColor:'orange'}}
-                                    className={classes.submit}
-                                >SIGNUP
-                                </Button></Link>
+                                    className={classes.submit}>LOGIN
+                            </Button>
 
                             <Grid container >
-
-                                <Grid item>
-                                    <Typography>Already have an account? </Typography>
+                                <Grid item xs>
                                     <Link href="#" variant="body2" style={{color:'white'}}>
-                                        { " Sign In "}
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>Don't have an account? </Typography>
+                                    <Link href="#" variant="body2" style={{color:'white'}}>
+                                        Sign Up
                                     </Link>
 
                                 </Grid>
-                            </Grid>
-                            <Grid className={classes.ava1}>
-                                <Link href="https://accounts.google.com/AddSession?hl=en&continue=https://www.google.co.in/"><FunctionsIcon /></Link>
-                                <Link href="https://github.com/login"><GitHubIcon /></Link>
-                                <Link href="https://www.linkedin.com/login"><LinkedInIcon /></Link>
                             </Grid>
                             <Box mt={5}>
                                 <Copyright />
